3d97ace13f21df3cbe2a3262e941ab5d1116dee4
karllundgren <karl@sosgate.com>
2020-03-18 20:51:21 -0600

    Added routes for pizzas

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+    public function index(){
+
+        //$pizzas = Pizza::all();
+        //$pizzas = Pizza::orderBy('name', 'desc')->get();
+        //$pizzas = Pizza::where('type', 'hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
+
+
+        return view('pizzas.index', [
+            'pizzas' => $pizzas,
+            'name' => request('name')
+        ]);
+        //return "pizzas!";
+        //return ['name'=> 'veg pizzas', 'base' => 'classic'];
+    }
+
+    public function show($id){
+        
+        $pizza = Pizza::findOrFail($id);
+        
+        
+        return view('pizzas.show', ['pizza' => $pizza]);
+    }
+
+    public function create(){
+        return view('pizzas.create');
+    }
+
+    public function store(){
+        //error_log(request('name'));
+
+        $pizza = new Pizza();
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
+        
+        error_log($pizza);
+        $pizza->save();
+       
+        return redirect('/')->with('mssg', 'Thanks for your order');
+    }
+
+    public function destroy($id){
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+
+        return redirect('/pizzas');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', 'PizzaController@index');
+Route::get('/pizzas/create', 'PizzaController@create');
+Route::post('/pizzas','PizzaController@store');
+Route::get('/pizzas/{id}', 'PizzaController@show' );
+Route::delete('/pizzas/{id}', 'PizzaController@destroy');
+
